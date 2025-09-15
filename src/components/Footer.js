5c9bb3b5482f151a7738ac05c79cea8559bf6b74
MSGanesh23 <2300031569@kluniversity.in>
2025-09-15 11:33:44 +0530

    Edited Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,17 +13,15 @@ function Footer() {
   return (
     <Container fluid className="footer">
       <Row>
+        
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Soumyajit Behera</h3>
-        </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} MSGanesh</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/MSGanesh23"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -31,19 +29,10 @@ function Footer() {
                 <AiFillGithub />
               </a>
             </li>
+            
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/msganesh23/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -51,16 +40,16 @@ function Footer() {
                 <FaLinkedinIn />
               </a>
             </li>
-            <li className="social-icons">
+            {/* <li className="social-icons">
               <a
                 href="https://www.instagram.com/soumyajit4419"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
               >
-                <AiFillInstagram />
-              </a>
-            </li>
+                <AiFillInstagram /> */}
+              {/* </a>
+            </li> */}
           </ul>
         </Col>
       </Row>
